bdd88f96905de28cc5601c51b1279dc3e991cf47
Stephen Montague <stephen.d.montague@gmail.com>
2021-11-05 18:00:08 -0400

    Learning how to manipulate maps & other ways to create them

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red": "#ff0000",
-		"green": "#008000",
-	}
+	// Below approach is taken if we want to figure out later what we want to add to it
+	// var colors map[string]string
+
+	// Pretty much equivalent to the code above
+	colors := make(map[string]string)
+
+	// Can add something to the map with the code below
+	colors["white"] = "#ffffff"
+
+	// colors := map[string]string{
+	// 	"red": "#ff0000",
+	// 	"green": "#008000",
+	// }
+
+
+	// Built in delete function to remove something from a map
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
